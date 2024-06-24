c0fadd8a7def687cbd980d989009c9fddf170245
tarekrahman916 <tarekrahman237@gmail.com>
2024-06-24 07:06:20 +0600

    added some changes

--- src/app/(backend)/dashboard/services/new/page.tsx
+++ src/app/(backend)/dashboard/services/new/page.tsx
@@ -4,19 +4,14 @@ import { getData } from "@/lib/getData";
 import React from "react";
 
 export default async function NewService() {
-  // const categoriesData = await getData("categories");
+  const categoriesData = await getData("categories");
 
-  // const categories = categoriesData.map((category: { id; title }) => {
-  //   return {
-  //     id: category.id,
-  //     title: category.title,
-  //   };
-  // });
+  const categories = categoriesData.map((category: { id; title }) => {
+    return {
+      id: category.id,
+      title: category.title,
+    };
+  });
 
-  return (
-    <div>
-      <h1>Service</h1>
-      {/* <NewServiceForm categories={categories} /> */}
-    </div>
-  );
+  return <NewServiceForm categories={categories} />;
 }
