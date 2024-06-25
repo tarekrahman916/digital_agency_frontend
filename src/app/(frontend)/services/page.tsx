2e36ee14e5734cf57df49ab36a076393ed363b2c
tarekrahman916 <tarekrahman237@gmail.com>
2024-06-25 13:26:30 +0600

    vercel Debug

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -5,7 +5,6 @@ import { getData } from "@/lib/getData";
 
 export default async function page() {
   const services: Service[] = await getData("services");
-  console.log(services);
   return (
     <div className="pt-40 lg:px-20 px-5">
       <PrimaryHeading title="Our Services" />
@@ -13,12 +12,11 @@ export default async function page() {
         data-aos="fade-right"
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 pb-10"
       >
-        {services.map((service: Service) => (
-          // <Card key={service.id} service={service} />
-          <h1 key={service.id}>hello</h1>
-        ))}
+        {Array.isArray(services) &&
+          services.map((service: Service) => (
+            <Card key={service.id} service={service} />
+          ))}
       </div>
-      {/* <h1>Service page</h1> */}
     </div>
   );
 }
